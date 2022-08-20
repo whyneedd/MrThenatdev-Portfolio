4956d141f24a0d0faecef6d4c17426a351ac1418
Jordane <jordan2offshel@hotmaile.com>
2022-08-20 17:47:14 -0400

    Me

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,14 +16,24 @@ var tl;
 
 tl = gsap.timeline({ defauts: { ease: "power.out" } });
 
-function aboutSlider() {
+function Scroll() {
     window.addEventListener("scroll", () => {
-        if (this.scrollY > 0) {
-            tl.to(".about__data", { duraction: .5, x: "0%", stagger: .25, opacity: 1, })
+        if (this.scrollY > 100) {
+            tl.to(".about", { duraction: .5, x: "0%", stagger: .25, opacity: 1, })
+        }
+        if (this.scrollY > 200) {
+            tl.to(".skills", { duraction: .5, x: "0%", stagger: .25, opacity: 1, })
+
+        }
+        if (this.scrollY > 300) {
+            tl.to(".work", { duraction: .5, x: "0%", stagger: .25, opacity: 1, })
+        }
+        if (this.scrollY > 400) {
+            tl.to(".contact", { duraction: .5, x: "0%", stagger: .25, opacity: 1, })
         }
     })
 }
-aboutSlider();
+Scroll();
 
 
 var navHumbger = document.querySelector(".nav__humbger");
