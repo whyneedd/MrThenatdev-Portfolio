429494e80c95ea7211cf3ce0b1beb02543480356
Jordane <jordan2offshel@hotmaile.com>
2022-08-20 19:01:47 -0400

    Me

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,24 @@ function Scroll() {
 }
 Scroll();
 
+let cookieModal = document.querySelector(".cookie")
+let cancelCookieBtn = document.querySelector(".btn-cancel")
+let acceptCookieBtn = document.querySelector(".btn-accept")
+
+cancelCookieBtn.addEventListener("click", function (){
+    cookieModal.classList.remove("active")
+})
+acceptCookieBtn.addEventListener("click", function (){
+    cookieModal.classList.remove("active")
+    localStorage.setItem("cookieAccepted", "yes")
+})
+
+setTimeout(function (){
+    let cookieAccepted = localStorage.getItem("cookieAccepted")
+    if (cookieAccepted != "yes"){
+        cookieModal.classList.add("active")
+    }
+}, 2000)
 
 var navHumbger = document.querySelector(".nav__humbger");
 var menu = document.querySelector(".nav__list");
