e29d3203c0bc56c60a4bec52c0a3c53d8cd74623
Jordane <jordan2offshel@hotmaile.com>
2022-08-27 07:07:22 -0400

    Me

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,6 +62,17 @@ function loading() {
     setTimeout(show, 3500)
 }
 
+function ScrollToTop() {
+    window.addEventListener("scroll", () => {
+        if (this.scrollY > 0) {
+            document.querySelector(".btnScrollToTop").classList.add("active");
+        } else {
+            document.querySelector(".btnScrollToTop").classList.remove("active");
+        }
+    })
+}
+ScrollToTop();
+
 function btnScrollToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
