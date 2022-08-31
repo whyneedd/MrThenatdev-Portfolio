4a201cb00fc1f26dc70f9884fec1f347bb9056e5
Jordane <jordan2offshel@hotmaile.com>
2022-08-31 08:17:00 -0400

    Me

---
+++ assets/js/countdown.js
@@ -0,0 +1,24 @@
+var coutnow = new Date("Sept 5, 2022 00: 00:00").getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = coutnow - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+
+    document.getElementById("days").innerHTML = 0;
+    document.getElementById("hours").innerHTML = 0;
+    document.getElementById("minutes").innerHTML = 0;
+    document.getElementById("seconds").innerHTML = 0; 
+  }
+}, 1000);
