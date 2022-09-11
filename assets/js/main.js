239f3a8e35a070aff4e4046218894a30da8319a2
Jordane <jordan2offshel@hotmaile.com>
2022-09-11 06:54:23 -0400

    By Me

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@ function showpage() {
 }
 
 var typed = new Typed(".auto-type", {
-  strings: ["a Zetrox", "a Dev", "a Designer", "a Freelancer"],
+  strings: ["a ZeTrox", "a Dev", "a Designer", "a Freelancer"],
   typeSpeed: 150,
   backSpeed: 150,
   loop: true,
